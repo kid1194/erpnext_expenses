b7aa7fe4a6d5ee7aefbc1db739ce50e33cc32844
kid1194 <kid1194@gmail.com>
2024-03-09 03:11:52 +0300

    v1-Alpha3 release

--- expenses/public/js/expenses.bundle.js
+++ expenses/public/js/expenses.bundle.js
@@ -6,52 +6,6 @@
 */
 
 
-$(document).ready(function() {
-    function $isFn(v) { return typeof v === 'function'; }
-    try {
-        let id = 'core-polyfill';
-        function onload() {
-            Promise.wait = function(ms) {
-                return new Promise(function(resolve) {
-                    window.setTimeout(resolve, ms);
-                });
-            };
-            Promise.prototype.timeout = function(ms) {
-                return Promise.race([
-                    this,
-                    Promise.wait(ms).then(function() { throw new Error('Time out'); })
-                ]);
-            };
-        }
-        if (
-            $isFn(String.prototype.trim) && $isFn(String.prototype.includes)
-            && $isFn(String.prototype.startsWith) && $isFn(String.prototype.endsWith)
-            && $isFn(Array.prototype.includes) && $isFn(Function.prototype.bind)
-            && $isFn(window.Promise)
-        ) onload();
-        else {
-            let $el = document.getElementById(id);
-            if (!!$el) onload();
-            else {
-                $el = document.createElement('script');
-                $el.id = id;
-                $el.src = 'https://polyfill.io/v3/polyfill.min.js?features=String.prototype.trim%2CString.prototype.includes%2CString.prototype.startsWith%2CString.prototype.endsWith%2CArray.prototype.includes%2CFunction.prototype.bind%2CPromise';
-                $el.type = 'text/javascript';
-                $el.async = true;
-                $el.onload = onload;
-                document.getElementsByTagName('head')[0].appendChild($el);
-            }
-        }
-        Array.prototype.clear = function() {
-            if (this.length) this.splice(0, this.length);
-            return this;
-        };
-        XMLHttpRequest.prototype.clear = function() {
-            this.onload = this.onerror = this.onabort = this.ontimeout = null;
-        };
-    } catch(e) { alert(e.message); }
-});
-
 
 class LevelUpCore {
     destroy() {
